79bca54c41042bec6fdb5f3b911c64b65ac1230d
napolisiqueira <imfelipenapoli@gmail.com>
2025-06-17 08:53:27 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	deleted:    BackEnd-Python/README.md
    	new file:   BackEnd-Python/qualquer.py
    	modified:   BackEnd-Python/render-deploy.sh
    	modified:   README.md

--- BackEnd-Python/render-deploy.sh
+++ BackEnd-Python/render-deploy.sh
@@ -3,4 +3,11 @@
 set -e
 
 run flask --app src.app db upgrade
-run guiunicorn src.wsgi:app
+run guiunicorn src.wsgi:app 
+
+            0     1      2
+lista = ["stone", ", item2]
+
+indice = ramdom.randint(0,2)
+
+lista[indice]
